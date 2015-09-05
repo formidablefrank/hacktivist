a9c6b652472605dcdeedcf00bad7927e1a33be92
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-05 22:51:33 +0800

    add base styles for register page

--- application/views/registerpage.php
+++ application/views/registerpage.php
@@ -1,1 +1,19 @@
-register
+<section id="registerpage">
+	<div class="ui text container">
+		<div class="ui form segment">
+			<h3>Register your Organization</h3>
+
+			<div class="ui input field">
+				<input type="text" placeholder="Username or Email" required>
+			</div>
+			<div class="ui input field">
+				<input type="password" placeholder="Password" required>
+			</div>
+			<div class="ui right aligned container">
+				<button class="ui large submit icon labeled button">
+					<i class="sign in icon"></i> Login
+				</button>
+			</div>
+		</div>
+	</div>
+</section>
