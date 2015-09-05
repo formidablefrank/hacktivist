5dddc3b4c7ac6e2fd6315439286412ce30a7cd0a
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-05 17:26:41 +0800

    add js files and clean up html

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,5 @@
 
 	<!-- load javscript files and other dependencies -->
     <script type="text/javascript" src="<?php echo base_url('js/semantic.min.js'); ?>"></script>
-
-    <!-- load custom scripts -->
-    <script type="text/javascript" src="<?php echo base_url('js/sticky.js'); ?>"></script>
-
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,10 +3,7 @@
 <head>
     <title> <?php echo $title;?> </title>
 
-
     <!-- import styles and essential scripts -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/semantic.min.css'); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/styles.css'); ?>">
-    <script type="text/javascript" src="<?php echo base_url('js/jquery.min.js'); ?>"></script>
 </head>
 <body>
