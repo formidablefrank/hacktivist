3691f7e010528a6e4f6d865ac74d0c73043a5c51
Gabby Torres <torres.gabriellemarie@gmail.com>
2015-09-06 02:31:48 +0800

    start template for event pages

--- application/views/eventpage.php
+++ application/views/eventpage.php
@@ -1,11 +1,48 @@
-<h3><?php echo $event->event_name; ?></h3>
-<h4><?php echo $event->ngo_name; ?></h4>
-<p><?php echo $event->event_details; ?></p>
-<p><?php echo $event->event_address; ?></p>
-<p><?php echo date('M d, D h:i a', strtotime($event->event_start)); ?> - <?php echo date('M d, D h:i a', strtotime($event->event_end));?></p>
-<p><?php echo $event->event_tokens; ?> token(s)</p>
-<p><?php echo $event->event_limit;?> slots</p>
-<a href="<?php echo base_url('join/'.$user_id.'/'.$event->event_id) ?>">Join the Event</a>
+<section id-"eventpage">
+	<div class="ui centered grid container">
+		<div class="fourteen wide column">
+			<div class="ui segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="ui medium image">
+							<img src="<?php echo base_url('img/forever.png') ?>" alt="Main photo">
+						</div>
+						<div class="content">
+							<div class="header">
+								<h2><?php echo $event->event_name; ?></h2>
+							</div>
+							<div class="meta">
+								<?php echo $event->ngo_name; ?>
+							</div>
+							<div class="ui divider"></div>
+							<div class="description">
+								<p>What: <?php echo $event->event_details; ?></p>
+								<p>When: <?php echo date('M d, D h:i a', strtotime($event->event_start)); ?> 
+								- <?php echo date('M d, D h:i a', strtotime($event->event_end));?></p>
+								<p>Where: <?php echo $event->event_address; ?></p>
+							</div>
+							<div class="extra content">
+								<div class="ui label">
+									<i class="certificate icon"></i>
+									<?php echo $event->event_tokens; ?> token(s)
+								</div>
+								<div class="ui label">
+									<i class="users icon"></i>
+									<?php echo $event->event_limit;?> slots
+								</div>
+								<a href="<?php echo base_url('join/'.$user_id.'/'.$event->event_id) ?>" 
+								class="ui right floated button">
+									Join the Event
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 
 Milestones
 
