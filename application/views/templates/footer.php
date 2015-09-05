fa750b3cd24fb356c38fdb646501b0813d81883a
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-05 17:29:05 +0800

    add and inject jquery

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,6 @@
 
 	<!-- load javscript files and other dependencies -->
+    <script type="text/javascript" src="<?php echo base_url('js/jquery-2.1.4.min.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('js/semantic.min.js'); ?>"></script>
 </body>
 </html>
