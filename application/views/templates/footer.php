8b185d5c7e553df5e03772945b338a11066e66c2
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-05 23:36:21 +0800

    temporarily disable fb connect to prevent error popups

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,14 @@
 
 	<!-- load javscript files and other dependencies -->
-    <script type="text/javascript" src="<?php echo base_url('js/jquery-2.1.4.min.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('js/semantic.min.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('js/fbconnect.js'); ?>"></script>
-    <!-- load webfonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:700,400,300" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700,400" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="<?php echo base_url('js/jquery-2.1.4.min.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('js/semantic.min.js'); ?>"></script>
+
+	<!-- temporarily remove connecting from FB to disable error popups
+	<script type="text/javascript" src="<?php echo base_url('js/fbconnect.js'); ?>"></script>
+	-->
+
+	<!-- load webfonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:700,400,300" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700,400" rel="stylesheet" type="text/css">
 </body>
 </html>
