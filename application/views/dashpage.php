c61f1196f2d1fc01aa7f607fb90a552073dd3332
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-05 23:34:33 +0800

    fix incorrect merge

--- application/views/dashpage.php
+++ application/views/dashpage.php
@@ -50,21 +50,21 @@
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="fourteen wide mobile ten wide tablet ten wide computer column">
-		<div class="ui fluid segment">
-			<div class="ui green ribbon label">Featured Events</div>
-			<?php foreach ($userevents as $event) { ?>
-				<div class="ui vertical segment">
-					<h3><?php echo $event->event_name; ?></h3>
-					<h4><?php echo $event->ngo_name; ?></h4>
-					<p><?php echo $event->event_details; ?></p>
-					<p><?php echo date('M d, D H:i a', strtotime($event->event_start)); ?> - <?php echo date('M d, D H:i a', strtotime($event->event_end));?></p>
-					<p><?php echo $event->event_limit; ?> slots left</p>
-					<a href="<?php echo base_url('join/'.$user_id.'/'.$event->event_id) ?>">Join the Event</a>
-					<a href="<?php echo base_url('event/'.$event->event_id) ?>">More details</a>
-				</div>
-			<?php } ?>
+		<div class="fourteen wide mobile ten wide tablet ten wide computer column">
+			<div class="ui fluid segment">
+				<div class="ui green ribbon label">Featured Events</div>
+				<?php foreach ($userevents as $event) { ?>
+					<div class="ui vertical segment">
+						<h3><?php echo $event->event_name; ?></h3>
+						<h4><?php echo $event->ngo_name; ?></h4>
+						<p><?php echo $event->event_details; ?></p>
+						<p><?php echo date('M d, D H:i a', strtotime($event->event_start)); ?> - <?php echo date('M d, D H:i a', strtotime($event->event_end));?></p>
+						<p><?php echo $event->event_limit; ?> slots left</p>
+						<a href="<?php echo base_url('join/'.$user_id.'/'.$event->event_id) ?>">Join the Event</a>
+						<a href="<?php echo base_url('event/'.$event->event_id) ?>">More details</a>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
 	</div>
 </section>
