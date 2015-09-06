52a67357635a019a37a8e8cc28376503f1567d2b
Joshua Frankie Rayo <kean.franco07@gmail.com>
2015-09-06 10:12:07 +0800

    homepage content

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,17 +4,20 @@
 	</section>
 	<section class="alt">
 		<div class="ui container">
-			Easy access for environmentalists to lend a hand.
+			<h1>Get more participants</h1>
+			Leaflet is a one-stop common place for all agencies and individuals to host their own environmental events. No need to visit different websites to learn these events. Easy access for environmentalists to lend a hand and make their advocacies realized!
 		</div>
 	</section>
 	<section>
 		<div class="ui container">
-			Every organization contributing to save the environment can share their projects!
+		<h1>Get rewarded and recognized</h1>
+			Volunteers get points after joining an event that can be used to redeem smart items. They can also brag their achievements using social media so that they can encourage more people to participate in the events.
 		</div>
 	</section>
 	<section class="alt">
 		<div class="ui container">
-			Enlist as volunteers to earn tokens and redeem them for cool rewards!
+		<h1>Strengthen agency partnerships</h1>
+			Every organization contributing to save the environment can share their projects! We encourage agency partnerhip for events to further expand their network.
 		</div>
 	</section>
 </section>
