9d9e827648d195635047e03919251a9daea97162
Gabby Torres <torres.gabriellemarie@gmail.com>
2015-09-06 11:03:14 +0800

    add padding to homepage container

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -5,19 +5,19 @@
 	<section class="alt">
 		<div class="ui container">
 			<h1>Get more participants</h1>
-			Leaflet is a one-stop common place for all agencies and individuals to host their own environmental events. No need to visit different websites to learn these events. Easy access for environmentalists to lend a hand and make their advocacies realized!
+			<p>Leaflet is a one-stop common place for all agencies and individuals to host their own environmental events. No need to visit different websites to learn these events. Easy access for environmentalists to lend a hand and make their advocacies realized!</p>
 		</div>
 	</section>
 	<section>
 		<div class="ui container">
 		<h1>Get rewarded and recognized</h1>
-			Volunteers get points after joining an event that can be used to redeem smart items. They can also brag their achievements using social media so that they can encourage more people to participate in the events.
+			<p>Volunteers get points after joining an event that can be used to redeem smart items. They can also brag their achievements using social media so that they can encourage more people to participate in the events.</p>
 		</div>
 	</section>
 	<section class="alt">
 		<div class="ui container">
 		<h1>Strengthen agency partnerships</h1>
-			Every organization contributing to save the environment can share their projects! We encourage agency partnerhip for events to further expand their network.
+			<p>Every organization contributing to save the environment can share their projects! We encourage agency partnerhip for events to further expand their network.</p>
 		</div>
 	</section>
 </section>
