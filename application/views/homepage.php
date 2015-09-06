fd67e0dd07c28e8e4f796177abc5d8f31c211a1d
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-09-06 09:26:36 +0800

    add base formatting for homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,16 +1,20 @@
-<section id="main-page">
-	<div class="ui grid container">
-		<div class="ui image container">
-			<img src="<?php echo base_url('/img/logo-black.svg') ?>" alt="">
-		</div>
-		<div class="ui text container">
+<section id="homepage">
+	<section class="ui center aligned container">
+		<img src="<?php echo base_url('/img/logo-black.svg') ?>" alt="logo-black">
+	</section>
+	<section class="alt">
+		<div class="ui container">
 			Easy access for environmentalists to lend a hand.
 		</div>
-		<div class="ui text container">
+	</section>
+	<section>
+		<div class="ui container">
 			Every organization contributing to save the environment can share their projects!
 		</div>
-		<div class="ui text container">
-			Enlist as volunteers to earn tokens and redeem them for points!
+	</section>
+	<section class="alt">
+		<div class="ui container">
+			Enlist as volunteers to earn tokens and redeem them for cool rewards!
 		</div>
-	</div>
+	</section>
 </section>
