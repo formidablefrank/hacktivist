8e08ed6899db2d8ed7eb7e1f33cda06430cf140e
Joshua Frankie Rayo <kean.franco07@gmail.com>
2015-09-06 11:51:56 +0800

    create event insert

--- application/views/createevent.php
+++ application/views/createevent.php
@@ -1,52 +1,59 @@
 <section id="create-event">
 	<div class="ui left aligned grid container">
 		<div class="ui segment container">
-			<form action="<? php echo base_url('/dashpage') ?>" class="ui form">
+			<?php echo form_open('create', array('class' => 'ui form', 'method' => 'POST')) ?>
 				<div class="ui dividing header">Promote your event!</div>
 				<div class="field">
 					<div class="label">Event name</div>
-					<input type="text" placeholder="Title">
+					<input type="text" placeholder="Title" name="title">
 				</div>
 				<div class="field">
 					<div class="label">About</div>
-					<textarea name="" id="" cols="30" rows="10" placeholder="What is it all about?"></textarea>
+					<textarea name="desc" id="" cols="30" rows="8" placeholder="What is it all about?"></textarea>
 				</div>
 				<div class="two fields">
 					<div class="field">
 						<div class="label">When</div>
-						<input type="date">
+						<input type="date" name="date">
 					</div>
 					<div class="field">
 						<div class="label">Where</div>
-						<input type="text">
+						<input type="text" name="address">
+						<input type="hidden" name="lat" value="15.5175512">
+						<input type="hidden" name="lon" value="120.5963779">
+					</div>
+				</div>
+				<div class="two fields">
+					<div class="field">
+						<div class="label">Number of participants</div>
+						<input type="number" name="limit">
+					</div>
+					<div class="field">
+						<div class="label">Number of tokens</div>
+						<input type="number" name="token">
 					</div>
 				</div>
 				<div class="two fields">
 					<div class="field">
 						<div class="label">Category</div>
-						<select class="ui dropdown">
+						<select class="ui dropdown" name="">
 							<option value="">Choose here</option>
-							<option value="Wildlife">Wildlife</option>
-							<option value="Pollution">Pollution</option>
-							<option value="Wastemanagement">Waste Management</option>
-							<option value="renewableenergy">Renewable Energy</option>
-							<option value="greentech">Green Technology</option>
+							<?php foreach ($categories as $category) { ?>
+								<option value="<?php echo $category->category_id ?>"><?php echo $category->category_name ?></option>
+							<?php } ?>
 						</select>
 					</div>
 					<div class="field">
 						<div class="label">For Featured Events</div>
-						<select class="ui dropdown">
+						<select class="ui dropdown" name="category">
 							<option value="">Choose here</option>
 							<option value="no">I won't pay for feature advertisements</option>
 							<option value="oneweek">1 week</option>
 							<option value="threeweeks">3 weeks</option>
 						</select>
 					</div>
-					<div class="field">
-						<div class="label"></div>
-					</div>
 				</div>
-				<a class="ui right floated submit button" href='<?php echo base_url('org')?>'>Submit</a>
+				<input type="submit"></a>
 			</form>
 		</div>
 	</div>
